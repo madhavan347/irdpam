76dad453a670e0b3585dc55d1377548873fb8a50
madhavan347 <madhavanshri.ms347@gmail.com>
2024-07-17 22:27:44 +0530

    added visitor count and brochure

--- index.js
+++ index.js
@@ -34,3 +34,70 @@ window.onload = function () {
   const launchDate = "2025-01-06T00:00:00";
   startTimer(launchDate);
 };
+
+const API_KEY =
+  "patv7fWYR8jIjZoMm.cb0e4030238467e1bbef146a639ace19548c27aa4afe53f6e28962c1287b8a0e";
+const AIRTABLE_BASE_ID = "appazeCJYcndSA6xa";
+const AIRTABLE_TABLE_NAME = "Table%201";
+const AIRTABLE_RECORD_ID = "rec8b933AlERY7YO0";
+
+function get_visitor_count() {
+  fetch(
+    `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}/${AIRTABLE_RECORD_ID}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((rawResponse) => {
+      let visitor_count = Number(rawResponse["fields"]["counter"]) + 1;
+      // console.log(rawResponse["fields"]["counter"]);
+      fetch(
+        `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}/`,
+        {
+          method: "PATCH",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+          },
+          body: JSON.stringify({
+            records: [
+              {
+                id: "rec8b933AlERY7YO0",
+                fields: {
+                  Name: "irpam_visitor_count",
+                  counter: visitor_count,
+                },
+              },
+            ],
+          }),
+        }
+      ).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      });
+      const visitor_element = document.getElementById("ogeyy");
+      visitor_element.innerHTML = visitor_count;
+    })
+    .catch((error) => {
+      console.error("Something went wrong.");
+      console.log("Error", error);
+    });
+}
+
+get_visitor_count();
